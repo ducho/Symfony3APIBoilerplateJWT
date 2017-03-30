5e7b22141d61f2e3d951c2e2c05c2b94f0b17bb8
Tony133 <a.tripodi133@gmail.com>
2017-03-30 16:20:19 +0200

    Update HelloControllerTest.php

--- tests/AppBundle/Controller/HelloControllerTest.php
+++ tests/AppBundle/Controller/HelloControllerTest.php
@@ -3,15 +3,28 @@
 namespace Tests\AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use GuzzleHttp\HandlerStack;
+use GuzzleHttp\Client;
+use GuzzleHttp\Handler\MockHandler;
+use GuzzleHttp\Psr7\Response;
 
 class HelloControllerTest extends WebTestCase
 {
+    protected function setUp()
+    {
+        $mock = new MockHandler([new Response(200, [])]);
+        $handler = HandlerStack::create($mock);
+        $this->client = new Client(['handler' => $handler]);
+	}
+
     public function testIndex()
     {
-        $client = static::createClient();
+        $response = $this->client->get('/', [
+            'json' => [
+                'hello' => 'This is a simple example of resource returned by your APIs'
+            ]
+        ]);
 
-        $crawler = $client->request('GET', '/');
-
-        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $this->assertEquals(200, $response->getStatusCode());
     }
 }
